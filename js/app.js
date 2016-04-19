5840ec38c471fa0b6b8063a9fd7f8806c621548b
Rakesh Patel <emailme@rakeshrpatel.com>
2016-04-19 18:08:53 +0530

    updated API, still troubleshooting multiple returns

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Not able to traverse the data properly. 
 var findArtistId = function(answer) {
     $.ajax({
         url: 'https://api.spotify.com/v1/search',
@@ -7,44 +8,49 @@ var findArtistId = function(answer) {
         },
 
         success: function(response) {
+            var artistName = response.artists.items[0].name;
+            $('.artistName').text(artistName);
+            console.log(artistName);
             var artistId = response.artists.items[0].id;
             findTopTracks(artistId);
         }
     });
 
-    var findTopTracks = function(artistId) {
-        $.ajax({
-            url: 'https://api.spotify.com/v1/artists/' + artistId + '/top-tracks',
-            data: {
-                country: 'US',
-            },
+};
+var findTopTracks = function(artistId) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/artists/' + artistId + '/top-tracks',
+        data: {
+            country: 'US',
+        },
 
-            success: function(data) {
-                $.each(data, function(i, item) {
-                    var finalResults = appendResults(item);
-                    console.log(data.item);
-                    $('.results').append(finalResults);
+        success: function(data) {
+            $('.artistName').text(data.tracks.name);
+            $.each(data.tracks, function(i, item) {
+                var finalResults = appendResults(item);
+                $('.results').append(finalResults);               
 
-
-                });
-            }
-        })
-    };
-
+            });
+        }
+    })
 };
 
-var appendResults = function(value) {
+var appendResults = function(item) {
+    
     var allMaterial = $('.results').clone;
 
-    var nameElem = $('.results').find('.name');
-    // nameElem.text(value.popularity);
-    
+    var songElem = $('.results').find('.name');
+    songElem.text('Song Name: ' + item.name);
+
     var popElem = $('.results').find('.popularity');
-    popElem.text('');
+    popElem.text('Popularity Score: ' + item.popularity);
 
     var linkElem = $('.results').find('.linkToSong');
-    linkElem.attr('href', );
-    
+    linkElem.attr('href', item.preview_url);
+
+    var imageElem=$('.results').find('.artistimage')
+    imageElem.attr('src', item.album.images[0].url);
+
     return allMaterial;
 }
 
